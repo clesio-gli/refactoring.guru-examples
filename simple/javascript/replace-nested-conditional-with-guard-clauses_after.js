77f1bb9524edec994dacb569a3f9cd2f488624fc
Olena Sovyn <Olena_Sovyn@epam.com>
2015-10-09 18:03:43 +0300

    Add js example for the replace nested conditional with guard clauses

---
+++ simple/javascript/replace-nested-conditional-with-guard-clauses_after.js
@@ -0,0 +1,14 @@
+function getPayAmount(self) {
+	if (self.isDead) {
+        return deadAmount();
+	}
+    if (self.isSeparated) {
+        return separatedAmount();
+    }
+    if (self.isRetired) {
+        return retiredAmount();
+    }
+
+    return normalPayAmount();
+}
+    
